256a6d873ead1304b69c2684811a3f662488e661
Rohan Mistry <rohandm99@yahoo.com>
2024-11-25 18:57:32 -0800

    Implemented observation space and reward function for environment.

--- scripts/traffic_env.py
+++ scripts/traffic_env.py
@@ -6,73 +6,82 @@ import json
 import os
 
 class TrafficSignalEnv(gym.Env):
-    def __init__(self, max_steps, config_file="../configs/basic/basic_config.json", road_network_file="../configs/basic/basic_road_network.json"):
+    def __init__(self, max_steps, config_file="../configs/basic/basic_config.json", road_network_file="../configs/basic/basic_road_network.json", centralized=False):
         super().__init__()
         self.max_steps = max_steps
+        self.centralized = centralized
         self.config_file = os.path.abspath(config_file)
         self.road_network_file = os.path.abspath(road_network_file)
         self.engine = cityflow.Engine(config_file, thread_num=1)
 
-        self.intersections, self.action_space, self.roads = self._parse_network(road_network_file)
+        self.intersections, self.roads, self.action_space = self._parse_network(road_network_file)
         self.num_intersections = len(self.intersections)
-        self.observation_space = spaces.Box(low=0, high=np.inf, shape=(self.num_intersections, 2), dtype=np.float32)
+
+        num_features = len(self.roads[next(iter(self.roads))]) * 2 + 1
+        if self.centralized:
+            self.observation_space = spaces.Box(low=0, high=np.inf, shape=(self.num_intersections * num_features,), dtype=np.float32)
+        else:
+            self.observation_space = spaces.Box(low=0, high=np.inf, shape=(self.num_intersections, num_features), dtype=np.float32)
     
     def reset(self):
         self.engine.reset()
+        self.previous_vehicle_count = self.engine.get_vehicle_count()
         return self._get_observations()
     
     def step(self, actions):
         self._apply_actions(actions)
         self.engine.next_step()
-        observations = self._get_observations()
+        observations = self._get_observations(self.centralized)
         rewards = self._get_rewards()
         done = self._is_done()
-
         return observations, rewards, done, {}
     
     def _parse_network(self, road_network_file):
         with open(road_network_file, "r") as f:
             data = json.load(f)
-        num_phases = max(len(intersection["trafficLight"]["lightphases"]) for intersection in data["intersections"])
-        lanes_by_road = {}
-        for road in data["roads"]:
-            lanes_by_road[road["id"]] = road["lanes"]
-        return data["intersections"], spaces.Discrete(num_phases), lanes_by_road
+        intersections = data["intersections"]
+        lanes_by_road = {road["id"]: road["lanes"] for road in data["roads"]}
+        num_phases = max(len(intersection["trafficLight"]["lightphases"]) for intersection in intersections)
+        return intersections, lanes_by_road, spaces.Discrete(num_phases)
     
     def _get_observations(self):
         states = []
-        lane_vehicle_counts = self.engine.get_lane_vehicle_count()
-        lane_waiting_times = self.engine.get_lane_waiting_time()
+        # Currently retrieves vehicle count and waiting vehicle count, can add waiting time and other potential features later (more complicated)
         for intersection in self.intersections:
             roads = intersection["roads"]
-            queue_lengths = []
-            waiting_times = []
+            vehicle_counts = []
+            waiting_vehicle_counts = []
 
             for road in roads:
                 lanes = self.roads[road]
-                queue_lengths.append(sum(lane_vehicle_counts.get(lane, 0) for lane in lanes))
-                waiting_times.append(sum(lane_waiting_times.get(lane, 0) for lane in lanes))
+                vehicle_counts.extend([self.engine.get_lane_vehicle_count().get(lane, 0) for lane in lanes])
+                waiting_vehicle_counts.extend([self.engine.get_lane_waiting_vehicle_count().get(lane, 0) for lane in lanes])
 
-            total_queue = sum(queue_lengths)
-            total_waiting = sum(waiting_times)
-            states.append([total_queue, total_waiting])
+            current_phase = self.engine.get_current_phase(intersection["id"])
+            state = vehicle_counts + waiting_vehicle_counts + [current_phase]
+            states.append(state)
         
-        return np.array(states)
+        # Return global state (combine all intersection observations) if centralized, else return local states (one observation per intersection)
+        return np.array(states).flatten() if self.centralized else np.array(states)
     
     def _apply_actions(self, actions):
         intersection_ids = [intersection["id"] for intersection in self.intersections]
         for i, action in enumerate(actions):
-            self.engine.set_traffic_light_phase(intersection_ids[i], action)
+            self.engine.set_tl_phase(intersection_ids[i], action)
     
     def _get_rewards(self):
         rewards = []
+        lane_waiting_counts = self.engine.get_lane_waiting_vehicle_count()
+        throughput = (self.previous_vehicle_count - self.engine.get_vehicle_count()) / self.num_intersections
+
         for intersection in self.intersections:
-            waiting_time = sum(self.engine.get_lane_waiting_time(intersection["id"]).values())
-            queue_length = sum(self.engine.get_lane_vehicle_count(intersection["id"]).values())
-            throughput = sum(self.engine.get_lane_vehicle_count(intersection["id"]).values())
-            
-            reward = -0.5 * waiting_time - 0.5 * queue_length + 1.0 * throughput
+            roads = intersection["roads"]
+            total_waiting_time = sum(lane_waiting_counts.get(lane, 0) for road in roads for lane in self.roads[road])
+            total_vehicle_count = sum(self.engine.get_lane_vehicle_count().get(lane, 0) for road in roads for lane in self.roads[road])
+            reward = -0.5 * total_waiting_time - 0.5 * total_vehicle_count + 1.0 * throughput
             rewards.append(reward)
+        
+        self.previous_vehicle_count = self.engine.get_vehicle_count()
         return np.array(rewards)
     
     def _is_done(self):
@@ -82,12 +91,12 @@ if __name__ == "__main__":
     config_file = "../configs/basic/basic_config.json"
     road_network_file = "../configs/basic/basic_road_network.json"
     max_steps = 100
-    environment = TrafficSignalEnv(max_steps, config_file, road_network_file)
+    environment = TrafficSignalEnv(max_steps, config_file, road_network_file, False)
 
     observations = environment.reset()
     done = False
 
     while not done:
-        actions = environment.action_space.sample()
+        actions = [environment.action_space.sample() for _ in range(environment.num_intersections)]
         observations, rewards, done, info = environment.step(actions)
         print(f"Actions: {actions}, Observations: {observations}, Rewards: {rewards}")
