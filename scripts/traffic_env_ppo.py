e4b65c5efc3468104de8c72fc2a37b69cc4f09d9
Derek Xu <derekxu2016k@gmail.com>
2024-11-26 21:11:52 -0800

    Added PPO training

--- scripts/traffic_env_ppo.py
+++ scripts/traffic_env_ppo.py
@@ -4,4 +4,13 @@ from stable_baselines3.common.env_checker import check_env
 
 # Check the custom environment for compatibility issues
 env = TrafficSignalEnv(max_steps=1000, centralized=False)  # Adjust max_steps or centralized based on preference
+
 check_env(env)
+
+model = PPO(policy='MlpPolicy',env=env)
+
+model.learn(total_timesteps=10000)
+
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
+
+print(f"Mean reward: {mean_reward} +/- {std_reward}")
